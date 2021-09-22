3c67689bd941a7782ffd20a7eb2c700d7d6f9af0
Neelam Vishvakarma <neelam1992vishvakarma@gmail.com>
2021-09-22 17:22:37 +0530

    practice learning of Angular

--- ANGULAR-LEARNING/my-first-app/src/app/app.component.ts
+++ ANGULAR-LEARNING/my-first-app/src/app/app.component.ts
@@ -6,16 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular app';
-  #name = "neelam"
-  items=[];
-  newTask;
-  addToList() {
-    if (this.newTask == '') {
-    }
-    else {
-        this.items.push(this.newTask);
-        this.newTask = '';
-    }
-}
-}
+  
+  }
+    
+
+
